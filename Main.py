809c76a3a2dc381b1c7d1b82443adf386165aad7
ShashwatAryal <shashwat221@gmail.com>
2020-03-02 20:28:30 +1100

    adding basic file that retrives the time left for anime release date from https://anilist.co/

---
+++ Main.py
@@ -0,0 +1,16 @@
+import requests
+from lxml import html
+
+def get_release_countdown(url):
+	r  = requests.get(url)
+	timer = html.fromstring(r.content).xpath('normalize-space(//*[@id="app"]/div[3]/div/div[2]/div[1]/div[2]/div[1]/div[2]/span)')
+	return timer
+
+
+urls  = [('Boruto: Naruto Next Generations', 'https://anilist.co/anime/97938/Boruto-Naruto-Next-Generations'),
+		 ('Boku no Hero Academia', 'https://anilist.co/anime/104276/Boku-no-Hero-Academia-4'),
+		 ('Nanatsu no Taizai: Kamigaki no Gekirin','https://anilist.co/anime/108928/Nanatsu-no-Taizai-Kamigami-no-Gekirin')]
+
+
+for item in urls:
+	print (item[0], " " , get_release_countdown(item[1]))
