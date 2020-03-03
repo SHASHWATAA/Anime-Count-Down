1a683c4099d48df97d9e4f5c4c9bc8b511dce812
ShashwatAryal <shashwat221@gmail.com>
2020-03-04 04:55:27 +1100

    added slideshow

--- Main.py
+++ Main.py
@@ -21,8 +21,7 @@ def get_release_countdown(url):
 
 urls  = [('Boruto: Naruto Next Generations', 'https://www.monthly.moe/anime/1048-boruto-naruto-next-generations', 'https://myanimelist.net/anime/34566/Boruto__Naruto_Next_Generations/pics'),
 		 ('Boku no Hero Academia', 'https://www.monthly.moe/anime/2082-boku-no-hero-academia-2019', 'https://myanimelist.net/anime/38408/Boku_no_Hero_Academia_4th_Season/pics'),
-		 ('Nanatsu no Taizai: Kamigaki no Gekirin','https://www.monthly.moe/anime/2117-nanatsu-no-taizai-kamigami-no-gekirin','https://myanimelist.net/anime/39701/Nanatsu_no_Taizai__Kamigami_no_Gekirin/pics'),
-		 ('One Piece','https://www.monthly.moe/anime/181-one-piece','https://myanimelist.net/anime/21/One_Piece/pics')]
+		 ('Nanatsu no Taizai: Kamigaki no Gekirin','https://www.monthly.moe/anime/2117-nanatsu-no-taizai-kamigami-no-gekirin','https://myanimelist.net/anime/39701/Nanatsu_no_Taizai__Kamigami_no_Gekirin/pics')]
 
 
 for item in urls:
--- index.php
+++ index.php
@@ -7,42 +7,43 @@
 
 	<?php
 
-		$pythonOutput = shell_exec ( '/Applications/MAMP/htdocs/Anime-Count-Down/Main.py' );
-		// $pythonOutput = "Boruto: Naruto Next Generations;147;8 March 2020 17:30;['https://cdn.myanimelist.net/images/anime/4/83369.jpg', 'https://cdn.myanimelist.net/images/anime/12/86784.jpg', 'https://cdn.myanimelist.net/images/anime/1393/96860.jpg', 'https://cdn.myanimelist.net/images/anime/1777/104957.jpg']|Boku no Hero Academia;84;7 March 2020 17:30;['https://cdn.myanimelist.net/images/anime/1978/95162.jpg', 'https://cdn.myanimelist.net/images/anime/1251/97634.jpg', 'https://cdn.myanimelist.net/images/anime/1831/102539.jpg', 'https://cdn.myanimelist.net/images/anime/1315/102961.jpg', 'https://cdn.myanimelist.net/images/anime/1137/105203.jpg', 'https://cdn.myanimelist.net/images/anime/1023/105559.jpg', 'https://cdn.myanimelist.net/images/anime/1233/106246.jpg', 'https://cdn.myanimelist.net/images/anime/1744/106248.jpg']|Nanatsu no Taizai: Kamigaki no Gekirin;21;4 March 2020 17:55;['https://cdn.myanimelist.net/images/anime/1574/100519.jpg', 'https://cdn.myanimelist.net/images/anime/1546/103418.jpg']";
+	$pythonOutput = shell_exec ( '/Applications/MAMP/htdocs/Anime-Count-Down/Main.py' );
+	// $pythonOutput = "Boruto: Naruto Next Generations;147;8 March 2020 17:30;['https://cdn.myanimelist.net/images/anime/4/83369.jpg', 'https://cdn.myanimelist.net/images/anime/12/86784.jpg', 'https://cdn.myanimelist.net/images/anime/1393/96860.jpg', 'https://cdn.myanimelist.net/images/anime/1777/104957.jpg']|Boku no Hero Academia;84;7 March 2020 17:30;['https://cdn.myanimelist.net/images/anime/1978/95162.jpg', 'https://cdn.myanimelist.net/images/anime/1251/97634.jpg', 'https://cdn.myanimelist.net/images/anime/1831/102539.jpg', 'https://cdn.myanimelist.net/images/anime/1315/102961.jpg', 'https://cdn.myanimelist.net/images/anime/1137/105203.jpg', 'https://cdn.myanimelist.net/images/anime/1023/105559.jpg', 'https://cdn.myanimelist.net/images/anime/1233/106246.jpg', 'https://cdn.myanimelist.net/images/anime/1744/106248.jpg']|Nanatsu no Taizai: Kamigaki no Gekirin;21;4 March 2020 17:55;['https://cdn.myanimelist.net/images/anime/1574/100519.jpg', 'https://cdn.myanimelist.net/images/anime/1546/103418.jpg']";
 
-		$arrAllAnime = explode("|", $pythonOutput);
+	$arrAllAnime = explode("|", $pythonOutput);
 
 		// print_r($arrAllAnime);
 
-		echo "<div id = AnimeList>";
+	echo "<div id = AnimeList>";
 
-		foreach ($arrAllAnime as $EachAnime) {
-			$arrEachAnime = explode(";", $EachAnime);
-			echo ("<div class='Anime'>");
+	foreach ($arrAllAnime as $EachAnime) {
+		$arrEachAnime = explode(";", $EachAnime);
+		echo ("<div class='Anime'>");
 
-			$searchval = array("]","[","'");
-			$replaceval = array("","","");
-			$imagelinks = str_replace($searchval,$replaceval,$arrEachAnime[3]);
-			$imagelink = explode(',', $imagelinks);
+		$searchval = array("]","[","'");
+		$replaceval = array("","","");
+		$imagelinks = str_replace($searchval,$replaceval,$arrEachAnime[3]);
+		$imagelinksarr = explode(',', $imagelinks);
+		echo("<h1>");
+		echo $arrEachAnime[0];
+		echo( "</h1>");
 
-			
-
-			echo("<h1>");
-			echo $arrEachAnime[0];
-			echo( "</h1>");
-
-			echo ("<img src ='" . $imagelink[0] . "'>");
-			echo("<span class = 'Episode'>Ep: <span name='EpisodeNumber'>");
-			echo $arrEachAnime[1];
-			echo("</span></span><span name ='date'> Release Date: <span name='ReleaseDate'>");
-			echo $arrEachAnime[2];
-			echo("</span></span><span name ='countdown'> Time left: <span name='Timer'>");
-			echo("</span></span></div>");
+		foreach ($imagelinksarr as $imagelinks) {
+			echo('<img class="mySlides" src="' . $imagelinks . '">');
 		}
 
-		echo "</div>"
-	?>
+		echo("<span class = 'Episode'>Ep: <span name='EpisodeNumber'>");
+		echo $arrEachAnime[1];
+		echo("</span></span><span name ='date'> Release Date: <span name='ReleaseDate'>");
+		echo $arrEachAnime[2];
+		echo("</span></span><span name ='countdown'> Time left: <span name='Timer'>");
+		echo("</span></span></div>");
+	}
 
+	echo "</div>"
+	?>
+	
+	<!-- timer script -->
 	<script>
 
 		var AnimeList = document.getElementById('AnimeList').getElementsByTagName('div');
@@ -63,38 +64,89 @@
 
 		// Update the count down every 1 second
 		var x = setInterval(Counter, 1000, Anime, ReleaseDateUTCepoch);	
-	
-		}
 
-		function Counter(Anime, ReleaseDateUTCepoch) {
+	}
 
-		// Get today's date and time
-		var d = new Date();
-		var localTime = d.getTime();
-		var localOffset = d.getTimezoneOffset() * 60000;
-		var UTClocal = localTime + localOffset;
+	function Counter(Anime, ReleaseDateUTCepoch) {
 
-		// Find the distance between now and the count down date
-		var distance = ReleaseDateUTCepoch - UTClocal;
+			// Get today's date and time
+			var d = new Date();
+			var localTime = d.getTime();
+			var localOffset = d.getTimezoneOffset() * 60000;
+			var UTClocal = localTime + localOffset;
 
-		// Time calculations for days, hours, minutes and seconds
-		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-		// Display the result in the element with id="demo"
-		Anime.querySelector("span[name='Timer']").innerHTML = days + "d " + hours + "h "
-		+ minutes + "m " + seconds + "s ";
+			// Find the distance between now and the count down date
+			var distance = ReleaseDateUTCepoch - UTClocal;
 
-		// If the count down is finished, write some text 
-		if (distance < 0) {
-			clearInterval(x);
-			Anime.getElementById("demo").innerHTML = "EXPIRED";
-		}
+			// Time calculations for days, hours, minutes and seconds
+			var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+			// Display the result in the element with id="demo"
+			Anime.querySelector("span[name='Timer']").innerHTML = days + "d " + hours + "h "
+			+ minutes + "m " + seconds + "s ";
+
+			// If the count down is finished, write some text 
+			if (distance < 0) {
+				clearInterval(x);
+			}
 		}
 	</script>
 
-</body>
+
+	<!--slideshow script-->
+
+	<script>
+		
+		// Animes = document.querySelectorAll(".Anime");
+
+		// for (Anime of Animes){
+
+		// 	Images = Anime.querySelectorAll("img");
+		// 	var slideIndex = 0;
+		// 	slideshow(Images);
+		// }
+
+		
+		var slideIndex = Array();
+		Animes = document.querySelectorAll(".Anime")
+
+		Anime = Animes.length - 1; 
+		for (var i = 0 ; i <= Anime; i++) {
+			slideIndex[i] = 0;
+		}
+
+		slideshow();
+
+
+
+		function slideshow(){
+
+			for (var i = 0 ; i <= Anime; i++) {
+				Images = Animes[i].querySelectorAll("img");
+
+				for (var j = 0; j <= Images.length - 1; j++) {
+					if (j != slideIndex[i]) {Images[j].hidden = true;}
+					else{Images[j].hidden = false;}
+				}
+			}
+
+
+			for (var i = 0 ; i <= Anime; i++) {
+				slideIndex[i]++;
+				if(slideIndex[i] > Animes[i].querySelectorAll("img").length -1 ){slideIndex[i] = 0;}
+			}
+
+	  		setTimeout(slideshow, 2000); // Change image every 2 seconds
+	  	}
+
+
+
+
+	  </script>
+
+	</body>
 
 
 
