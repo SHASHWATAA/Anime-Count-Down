defd9b07a42fa2b1210bea463cbb1e3cc97f65b1
ShashwatAryal <shashwat221@gmail.com>
2020-03-03 19:18:28 +1100

    fixed bugs
    python now sends just raw data and not html data tags. raw data is parsed in php and php sends html

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@
 
 import requests
 from lxml import html
+phptransfer=''
 
 def get_release_countdown(url):
 	r  = requests.get(url)
@@ -17,4 +18,11 @@ urls  = [('Boruto: Naruto Next Generations', 'https://www.monthly.moe/anime/2117
 
 
 for item in urls:
-	print ("<p><h1>", item[0], "</h1> episode:" , get_release_countdown(item[1])[1][0], "release date:", get_release_countdown(item[1])[0][0] ,"</p>")
+	# print ("<p id ='t'><span>", item[0], "</span> <span>episode:" , get_release_countdown(item[1])[1][0], "</span><span>release date:", get_release_countdown(item[1])[0][0] ,"</span></p>")
+	name = item[0]
+	episodenum = get_release_countdown(item[1])[1][0]
+	releaseDate = get_release_countdown(item[1])[0][0].strip()
+	combined = name + ";" + episodenum + ";" + releaseDate
+	phptransfer = phptransfer + "|" + combined
+
+print(phptransfer[1:])
--- index.php
+++ index.php
@@ -1,7 +1,57 @@
 <?php
 
-$op = shell_exec ( '/Applications/MAMP/htdocs/Anime-Count-Down/Main.py' );
+// $op = shell_exec ( '/Applications/MAMP/htdocs/Anime-Count-Down/Main.py' );
+$pythonOutput = "Boruto: Naruto Next Generations;21;4 March 2020 17:55 JST (+09:00)|Boku no Hero Academia;147;8 March 2020 17:30 JST (+09:00)|Nanatsu no Taizai: Kamigaki no Gekirin;147;8 March 2020 17:30 JST (+09:00)";
 
+$arrAllAnime = explode("|", $pythonOutput);
 
-echo $op;
+// print_r($arr);
+
+foreach ($arrAllAnime as $EachAnime) {
+	$arrEachAnime = explode(";", $EachAnime);
+	echo ("<p name='Anime'><h1><span name = 'Title'>");
+	echo $arrEachAnime[0];
+	echo( "</span></h1> <span name = 'Episode'>Ep: <span name='EpisodeNumber'>");
+	echo $arrEachAnime[1];
+	echo("</span></span><span name ='date'> Release Date: <span name='ReleaseDate'>");
+	echo $arrEachAnime[2];
+	echo("</span></span></p>");
+}
+
 ?>
+
+
+<!-- Display the countdown timer in an element -->
+<p id="demo"></p>
+
+<script>
+// Set the date we're counting down to
+var ddate= document.querySelector('body > span:nth-child(4) > span').innerText;
+var countDownDate = new Date("ddate").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
